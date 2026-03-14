f7f6123393f7f6e49f446c2740b4b6d5cd543d19
morad <KARKARMORAD29@GMAIL.COM>
2026-03-14 10:53:49 +0100

    posts.router

--- routers/posts.js
+++ routers/posts.js
@@ -1,93 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const posts = require('../data/posts');
+const postsController = require('../data/controllers/postscontroller');
 
+//Definizione delle rotte per i post
 
-// (Get) Tutti i post
-router.get('/', (req, res) => {
-  res.json(posts);
-
-
-});
+// (Get) Tutti i post con filtro per tag
+router.get('/', postsController.index);
 
 // (Get) Singolo post per ID
-router.get('/ :id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = posts.find(p => p.id === id);
-
-
-  //Retituisco il post trovatp
-  if (!post) {
-    return res.status(404).json({ error: "Post non trovato" });
-  }
-  res.json(post);
-});
+router.get('/:id', postsController.show);
 
 //Create - Crea un post
-router.post('/', (req, res) => {
-  console.log("Creazione di un nuovo post con i dati:", req.body);
-
-  const { title, content, image, tags } = req.body;
-
-  if (!title || !content || !image || !tags) {
-    return res.status(400).json({ error: "Tutti i campi sono obbligatori" });
-  }
-  //Generazione ID univoco
-  const lastIndex = posts.length > 0 ? posts[posts.length - 1].id : 0;
-  const newId = lastIndex + 1;
-
-  const newPost = {
-    id: newId,
-    title,
-    content,
-    image,
-    tags
-  };
-
-  posts.push(newPost);
-
-  res.status(201).json(newPost);
-});
+router.post('/', postsController.store);
 
 //Update - Aggiorna un post
-router.put('/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const postIndex = posts.findIndex(p => p.id === id);
-
-  //Controllo se il post esiste
-  if (postIndex === -1) {
-    return res.status(404).json({ error: "Post non trovato" });
-  }
-
-  const { title, content, image, tags } = req.body;
-
-  if (!title || !content || !image || !tags) {
-    return res.status(400).json({ error: "Tutti i campi sono obbligatori" });
-  }
-
-  posts[postIndex] = {
-    ...posts[postIndex],
-    title,
-    content,
-    image,
-    tags
-  };
-
-  res.json(posts[postIndex]);
-});
+router.put('/:id', postsController.update);
 
 //Delete - Elimina un post
-router.delete('/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const postIndex = posts.findIndex(p => p.id === id);
-
-  if (postIndex === -1) {
-    return res.status(404).json({ error: "Post non trovato" });
-  }
-  posts.splice(postIndex, 1);
-  res.json({ message: "Post eliminato con successo" });
-});
-
+router.delete('/:id', postsController.destroy);
 
 
 
